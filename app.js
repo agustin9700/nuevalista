b4837a8c1d38c54064e41760f68506eec7fe7631
agustin9700 <137729900+agustin9700@users.noreply.github.com>
2024-01-29 21:34:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ async function extraerDatosYGuardarJson(url, nombreArchivo) {
 }
 
 async function ejecutarProcesoVariasVeces() {
-    const url = 'https://ninjakaizen.com/clan/101';
+    const url = 'https://ninjakaizen.com/clan/148';
 
     for (let i = 0; i < 999; i++) {
         const nombres = ['datos.json', 'newdatos.json'];
